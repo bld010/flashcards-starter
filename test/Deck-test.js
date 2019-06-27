a28b470ac01426cc5ef2a80f347dcad7c6608cd2
Brianna DelValle <briannadelvalle@gmail.com>
2019-06-27 15:43:27 -0600

    Refactor deck-test to include beforeEach and rename it blocks

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -4,32 +4,30 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 
+var deck, card1, card2, card3;
 
 describe('Deck', function() {
 
+  beforeEach(function() {
+    card1 = new Card({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
+    card2 = new Card({id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder'});
+    card3 = new Card({id: 12, question: 'What is Travis\'s middle name?', answers: ['Lex', 'William', 'Fitzgerald'], correctAnswer: 'Fitzgerald'});
+    deck = new Deck([card1, card2, card3]);
+  })
+
   it('should be a function', function() {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
-    const deck = new Deck();
+  it('should create instances of Deck', function() {
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should store cards', function() {
-    const card1 = new Card({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
-    const card2 = new Card({id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder'});
-    const card3 = new Card({id: 12, question: 'What is Travis\'s middle name?', answers: ['Lex', 'William', 'Fitzgerald'], correctAnswer: 'Fitzgerald'});
-    const deck = new Deck([card1, card2, card3]);
     expect(deck.cards).to.deep.equal([card1, card2, card3]);
   })
 
-  it('should have a method to count cards', function() {
-    const card1 = new Card({id: 1, question: 'What is Robbie\'s favorite animal', answers: ['sea otter', 'pug', 'capybara'], correctAnswer: 'sea otter'});
-    const card2 = new Card({id: 14, question: 'What organ is Khalid missing?', answers: ['spleen', 'appendix', 'gallbladder'], correctAnswer: 'gallbladder'});
-    const card3 = new Card({id: 12, question: 'What is Travis\'s middle name?', answers: ['Lex', 'William', 'Fitzgerald'], correctAnswer: 'Fitzgerald'});
-    const deck = new Deck([card1, card2, card3]);
+  it('should count cards', function() {
     expect(deck.countCards()).to.equal(3);
   });
 });
