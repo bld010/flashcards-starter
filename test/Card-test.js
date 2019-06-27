5f66f5db150fc1ce6ff0702f02221b887739f02d
Brianna DelValle <briannadelvalle@gmail.com>
2019-06-27 15:45:18 -0600

    Refactor card-test to include beforeEach hook and rename it blocks

--- test/Card-test.js
+++ test/Card-test.js
@@ -3,30 +3,32 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 
+var card;
+
 describe('Card', function() {
 
+  beforeEach(function() {
+    card = new Card({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
+  });
+
   it('should be a function', function() {
-    const card = new Card();
     expect(Card).to.be.a('function');
   });
 
-  it('should be an instance of Card', function() {
-    const card = new Card();
+  it('should create instances of Card', function() {
     expect(card).to.be.an.instanceof(Card);
   }); 
 
   it('should store a question', function() {
-    const card = new Card({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
   it('should store a list of possible answers', function() {
-    const card = new Card({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
   it('should store the correct answer', function() {
-    const card = new Card({id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: ['object', 'array', 'function'], correctAnswer: 'object'});
     expect(card.correctAnswer).to.equal('object');
   });
+  
 });
