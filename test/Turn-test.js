07fb83c074a14157e553deb68f1a4c829d2dd5d9
Brianna DelValle <briannadelvalle@gmail.com>
2019-06-27 15:40:27 -0600

    Refactor Turn-test to include beforeEach hooks and rename it blocks

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,52 +4,73 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
+var card, turn1;
+
 describe('Turn', function(){
 
+  beforeEach(function() {
+    card = new Card({id: 1, question: 'Where is Turing Located?', answers:['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'});
+    turn1 = new Turn('Denver', card);
+  });
+
   it('should be a function', function() {
-    const turn = new Turn();
     expect(Turn).to.be.a('function');
   });
 
-  it('should be an instance of Turn', function() {
-    const turn = new Turn();
-    expect(turn).to.be.an.instanceof(Turn);
+  it('should create instances of Turn', function() {
+    expect(turn1).to.be.an.instanceof(Turn);
   });
 
   it('should store a guess', function(){
-    const turn = new Turn('foo');
-    expect(turn.guess).to.equal('foo');
+
+    expect(turn1.guess).to.equal('Denver');
   });
 
   it('should store a card', function() {
-    const card = new Card({id: 1, question: 'Where is Turing Located?', answers:'Denver'});
-    const turn = new Turn('Denver', card);
-    expect(turn.card).to.deep.equal(card);
+
+    expect(turn1.card).to.deep.equal(card);
   });
 
-  it('should have a method to return guess', function() {
-    const card = new Card({id: 1, question: 'Where is Turing Located?', answers:'Denver'});
-    const turn = new Turn('Denver', card);
-    expect(turn.returnGuess()).to.equal('Denver');
+  it('should return guesses', function() {
+
+    expect(turn1.returnGuess()).to.equal('Denver');
   });
 
-  it('should have a method to return card', function() {
-    const card = new Card({id: 1, question: 'Where is Turing Located?', answers:['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'});
-    const turn = new Turn('Denver', card);
-    expect(turn.returnCard()).to.deep.equal({id: 1, question: 'Where is Turing Located?', answers: ['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'})
+  it('should return cards', function() {
+
+    expect(turn1.returnCard()).to.deep.equal({id: 1, question: 'Where is Turing Located?', answers: ['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'})
   });
 
-  it('should have a method to evaluate guess', function() {
-    const card = new Card({id: 1, question: 'Where is Turing Located?', answers:['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'});
-    const turn1 = new Turn('Denver', card);
-    expect(turn1.evaluateGuess()).to.equal(true);
+  describe('evaluateGuess', function() {
+
+    it('should evaluate correct guesses', function() {
+      expect(turn1.evaluateGuess()).to.equal(true);
+      const turn2 = new Turn('Chicago', card);
+      expect(turn2.evaluateGuess()).to.equal(false);
+    });
+
+    it('should evaluate incorrect guesses', function() {
+      const turn2 = new Turn('Chicago', card);
+      expect(turn2.evaluateGuess()).to.equal(false);
+    });
+
+  });
+
+  describe('giveFeedback', function() {
+
+    it('should give feedback on correct guesses', function() {
+    expect(turn1.giveFeedback()).to.equal('correct!');
+    });
+
+    it('should give feedback on incorrect guesses', function() {
     const turn2 = new Turn('Chicago', card);
-    expect(turn2.evaluateGuess()).to.equal(false);
-  })
+    expect(turn2.giveFeedback()).to.equal('incorrect!');
+    });
 
-  it('should give feedback on guess', function() {
-    const card = new Card({id: 1, question: 'Where is Turing Located?', answers:['Denver', 'Chicago', 'New York'], correctAnswer: 'Denver'});
-    const turn1 = new Turn('Denver', card);
+  });
+
+  it('should give feedback on guesses', function() {
+
     expect(turn1.giveFeedback()).to.equal('correct!');
     const turn2 = new Turn('Chicago', card);
     expect(turn2.giveFeedback()).to.equal('incorrect!');
